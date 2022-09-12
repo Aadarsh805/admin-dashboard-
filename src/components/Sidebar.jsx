ad6cb14d12deab8ac8cd8470f41b248042a4de42
aadarsh805 <aadarshthakur80501@gmail.com>
2022-09-12 22:11:37 +0530

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,9 +4,11 @@ import { MdOutlineCancel } from "react-icons/md";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import { links } from "../data/dummy";
+import {useStateContext} from '../contexts/ContextProvider'
 
 const Sidebar = () => {
-  const activeMenu = true;
+  const {activeMenu, setActiveMenu } = useStateContext()
+  
   const activeLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
 
@@ -20,7 +22,7 @@ const Sidebar = () => {
           <div className="flex justify-between items-center">
             <Link
               to="/"
-              onClick={() => {}}
+              onClick={() => setActiveMenu(false)}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:white text-slate-900"
             >
               <SiShopware /> <span>Shoppy</span>
@@ -28,7 +30,7 @@ const Sidebar = () => {
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
                 type="button"
-                onClick={() => {}}
+                onClick={() => setActiveMenu(prev => !prev)}
                 className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
               >
                 <MdOutlineCancel />
@@ -46,7 +48,7 @@ const Sidebar = () => {
                     key={link.name}
                     onClick={() => {}}
                     className={(isActive) =>
-                      isActive ? activeLink : normalLink
+                      isActive ? normalLink : activeLink
                     }
                   >
                     {link.icon}
