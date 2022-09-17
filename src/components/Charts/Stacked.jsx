9210a490693dfdd4eafefb2f2baa63abf22a1fb1
aadarsh805 <aadarshthakur80501@gmail.com>
2022-09-17 18:05:39 +0530

    customers page

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -8,11 +8,12 @@ const Stacked = ({width, height}) => {
     <ChartComponent 
       width={width}
       height={height}
-      id='stack chart'
+      id='charts'
       primaryXAxis={stackedPrimaryXAxis}
       primaryYAxis={stackedPrimaryYAxis}
       chartArea={{border: {width: 0}}}
       tooltip={{enable: true}}
+      legendSettings={{background: 'white'}}
     >
       <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
       <SeriesCollectionDirective>
--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import {GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter} from '@syncfusion/ej2-react-grids'
+
+import {customersData, customersGrid} from '../data/dummy'
+import {Header} from '../components'
 
 const Customers = () => {
   return (
     <div>Customers</div>
   )
-}
+} 
 
 export default Customers
